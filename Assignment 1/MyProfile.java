31f40069d53edd636ab07cc80d7448f611bb8d60
mikefallen <mike_7653@hotmail.com>
2014-11-20 23:51:18 -0500

    Assignment 1 Complete

---
+++ Assignment 1/MyProfile.java
@@ -0,0 +1,24 @@
+/*
+Name: Michael Zarglis
+Assignment 1
+Due Date: 9/18/2014 @ 11PM
+Description: Prints a short bio about me
+*/
+public class MyProfile {
+    
+    
+    public static void main(String[] args) { 
+        
+        int age = 22;
+		
+        System.out.println("Name: Michael Zarglis");
+        System.out.println("Age: " + age);
+        System.out.println("Favourite Movie: How to Train Your Dragon");
+        System.out.println("Favourite Food: Greek (All of it)");
+        System.out.println("Interests: Computers, Networks, Travelling, Motorcycles");
+        
+    }   
+    
+    
+    
+}
